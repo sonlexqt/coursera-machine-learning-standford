f4eafea62de3e8be465ab5255ae7635e62580fb5
Thai Son Le <Thai Son Le>
2016-03-08 18:11:20 +0700

    Finish ex 5

--- machine-learning-ex5-done/ex5/learningCurve.m
+++ machine-learning-ex5-done/ex5/learningCurve.m
@@ -53,11 +53,16 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
-
+for i = 1:m
+    % Compute train/cross validation errors using training examples 
+    % X(1:i, :) and y(1:i), storing the result in 
+    % error_train(i) and error_val(i)           
+    X_temp = X(1:i, :);
+    y_temp = y(1: i);
+    theta_temp = trainLinearReg(X_temp, y_temp, lambda);
+    error_train(i) = linearRegCostFunction(X_temp, y_temp, theta_temp, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta_temp, 0);
+end
 
 % -------------------------------------------------------------
 
--- machine-learning-ex5-done/ex5/linearRegCostFunction.m
+++ machine-learning-ex5-done/ex5/linearRegCostFunction.m
@@ -19,16 +19,14 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+cost = ((X * theta - y) .^ 2) / (2 * m);
 
+reg_term = sum(sum(theta(2:end, :) .^ 2)) * lambda / (2*m);
 
+J = sum(cost) + reg_term;
 
-
-
-
-
-
-
-
+grad = (X' * (X * theta - y)) / m + theta * (lambda / m);
+grad(1) = grad(1) - theta(1) * (lambda / m);
 
 % =========================================================================
 
